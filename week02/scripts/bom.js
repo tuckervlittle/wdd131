0f7d2860f751a72cfa2bc6d5b27a8a4ca8317451
tuckervlittle <tucker.little95@gmail.com>
2025-01-14 10:50:22 -1000

    update

---
+++ week02/scripts/bom.js
@@ -0,0 +1,30 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', function() {
+    
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+        li.textContent = input.value;
+        
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = '❌'
+        
+        li.appendChild(deleteButton);
+             
+        list.appendChild(li);
+            
+        input.value = '';
+        input.focus();
+            
+        deleteButton.addEventListener('click', function() {
+            list.removeChild(li);
+        }
+    )}
+    else {
+        alert('Please enter a chapter title');
+        input.focus();
+        return;
+    }
+});
